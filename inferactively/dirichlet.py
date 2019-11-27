ddac9be4e9e62842fdb32311f5d3c0f48a3dca08
alec-tschantz <tschantz.alec@gmail.com>
2019-11-27 21:57:19 +0000

    Added API

--- inferactively/dirichlet.py
+++ inferactively/dirichlet.py
@@ -11,3 +11,66 @@ __author__: Conor Heins, Alexander Tschantz, Brennan Klein
 class Dirichlet(object):
     def __init__(self):
         pass
+
+    def normalize(self):
+        pass
+
+    def is_normalized(self):
+        pass
+
+    def remove_zeros(self):
+        pass
+
+    def contains_zeros(self):
+        pass
+
+    def entropy(self, return_numpy=False):
+        pass
+
+    def log(self, return_numpy=False):
+        pass
+
+    def dot(self, other, return_numpy=False):
+        pass
+
+    def copy(self):
+        pass
+
+    @property
+    def ndim(self):
+        return None
+
+    @property
+    def shape(self):
+        return None
+
+    def __add__(self, other):
+        pass
+
+    def __radd__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __rsub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __rmul__(self, other):
+        pass
+
+    def __contains__(self, value):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, idx, value):
+        pass
+
+    def __repr__(self):
+        return "-"
+
