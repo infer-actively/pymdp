1feb466f2300f8335b8e2b19b473288c3e49b8cf
alec-tschantz <tschantz.alec@gmail.com>
2019-11-28 16:35:15 +0000

    added initalization for Categorical, as well as indexing, setting, and .shape property. Update test script with tests

--- inferactively/categorical.py
+++ inferactively/categorical.py
@@ -7,10 +7,101 @@ __author__: Conor Heins, Alexander Tschantz, Brennan Klein
 
 """
 
+import numpy as np
+
 
 class Categorical(object):
-    def __init__(self):
-        pass
+    """ A Categorical distribution
+
+    A discrete probability distribution that describes the possible results of a random variable
+    that can take on one of K possible categories.
+    Parameters are all in the range 0 to 1, and all sum to 1
+
+    This class assumes that the columns encode probability distributions
+    This means that multiple columns represents a set of distributions
+    TODO: Describe what is happening with arrays of arrays
+
+    ...
+
+    Attributes
+    ----------
+    values : np.ndarray
+        The parameters of the Categorical distribution
+    TODO:
+
+    Methods
+    -------
+    TODO:
+
+    """
+
+    def __init__(self, dims=None, values=None):
+        """Initialize a Categorical distribution
+
+        Note: _either_ dims or values should be provided, providing both will raise an error
+        Note: for single dimension :values:, the dimension is changed to [n x 1]
+        Note: if neither :dims: or :values: is provided, a `dims=[1, 1]` distribution is created
+        TODO: Describe what is happening with arrays of arrays
+
+        Parameters
+        ----------
+        dims: list of int
+            Specify the number and size of dimensions
+            This will initialize the parameters with zero values
+        values: np.ndarray
+            The parameters of the distribution
+
+        """
+
+        self.IS_AOA = False
+
+        if values is not None and dims is not None:
+            raise ValueError("please provide _either_ :dims: or :values:, not both")
+
+        if values is None and dims is None:
+            self.values = np.zeros([1, 1])
+
+        if values is not None:
+            if not isinstance(values, np.ndarray):
+                raise ValueError(":values: must be a :numpy.ndarray:")
+
+            if values.dtype == "object":
+                self.IS_AOA = True
+
+            if self.IS_AOA:
+                self.values = np.empty(len(values), dtype="object")
+                for i, array in enumerate(values):
+                    if array.ndim == 1:
+                        values[i] = np.expand_dims(values[i], axis=1)
+                    self.values[i] = values[i].astype("float64")
+            else:
+                if values.ndim == 1:
+                    values = np.expand_dims(values, axis=1)
+                self.values = values.astype("float64")
+
+        if dims is not None:
+            if isinstance(dims, list):
+                if any(isinstance(el, list) for el in dims):
+                    if not all(isinstance(el, list) for el in dims):
+                        raise ValueError("you have mixed :int: and :list: in your list of :dims:")
+                    self.IS_AOA = True
+
+                if self.IS_AOA:
+                    self.values = np.empty(len(dims), dtype="object")
+                    for i in range(len(dims)):
+                        if len(dims[i]) == 1:
+                            self.values[i] = np.zeros([dims[i][0], 1])
+                        else:
+                            self.values[i] = np.zeros(dims[i])
+                else:
+                    if len(dims) == 1:
+                        self.values = np.zeros([dims[0], 1])
+                    else:
+                        self.values = np.zeros(dims)
+            elif isinstance(dims, int):
+                self.values = np.zeros([dims, 1])
+            else:
+                raise ValueError(":dims: must be either :list: or :int:")
 
     def normalize(self):
         pass
@@ -42,7 +133,7 @@ class Categorical(object):
 
     @property
     def shape(self):
-        return None
+        return self.values.shape
 
     def __add__(self, other):
         pass
@@ -66,10 +157,21 @@ class Categorical(object):
         pass
 
     def __getitem__(self, key):
-        pass
+        values = self.values[key]
+        if isinstance(values, np.ndarray):
+            if values.ndim == 1:
+                values = values[:, np.newaxis]
+            return Categorical(values=values)
+        else:
+            return values
 
     def __setitem__(self, idx, value):
-        pass
+        if isinstance(value, Categorical):
+            value = value.values
+        self.values[idx] = value
 
     def __repr__(self):
-        return "-"
+        """ TODO:
+
+        """
+        return str(self.values)
--- test.py
+++ test.py
@@ -1,6 +1,54 @@
+import numpy as np
+
 from inferactively import Categorical, Dirichlet, test_function
 
+
 if __name__ == "__main__":
-    A = Categorical()
-    B = Dirichlet()
-    test_function()
+
+    print("== Testing initialization via `dims` ===")
+
+    A = Categorical(dims=4)
+    print("Initalized `dims=4` gives shape {}".format(A.shape))
+
+    A = Categorical(dims=4)
+    print("Initalized `dims=4` gives shape {}".format(A.shape))
+
+    A = Categorical(dims=[5, 4])
+    print("Initalized `dims=[5,4]` gives shape {}".format(A.shape))
+
+    A = Categorical(dims=[[5, 4], [4, 3]])
+    print(
+        "Initalized `dims=[[5,4], [4,3]]` gives shape {} while A[0] gives shape {}".format(
+            A.shape, A[0].shape
+        )
+    )
+
+    print("== Testing initialization via `values` ===")
+
+    values = np.random.rand(5, 4)
+    A = Categorical(values=values)
+    print("Initalized `values=rand(5, 4)` gives shape {}".format(A.shape))
+
+    values = np.random.rand(5)
+    A = Categorical(values=values)
+    print("Initalized `values=rand(5)` gives shape {}".format(A.shape))
+
+    values_1 = np.random.rand(5, 4)
+    values_2 = np.random.rand(4, 3)
+    values = np.array([values_1, values_2])
+    A = Categorical(values=values)
+    print(
+        "Initalized `values=(rand(5, 4), rand(4, 3))` gives shape {} where A[0] gives {}".format(
+            A.shape, A[0].shape
+        )
+    )
+
+    values_1 = np.random.rand(5)
+    values_2 = np.random.rand(4)
+    values = np.array([values_1, values_2])
+    A = Categorical(values=values)
+    print(
+        "Initalized `values=(rand(5), rand(4))` gives shape {} where A[0] gives {}".format(
+            A.shape, A[0].shape
+        )
+    )
