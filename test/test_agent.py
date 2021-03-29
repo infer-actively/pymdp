903b8dec91c1d8bd6f8d8fb120c4e15f544108c7
conorheins <conor.heins@gmail.com>
2021-03-29 17:57:34 +0200

    filled out AI loop for vbx ...still debugging

--- test/test_agent.py
+++ test/test_agent.py
@@ -82,25 +82,6 @@ class TestAgent(unittest.TestCase):
         Test against output of SPM_MDP_VB_X Case A - one hidden state factor, one observation modality, policy_len = 1
         """
 
-        array_path = os.path.join(os.getcwd(), DATA_PATH + "vbx_test_a.mat")
-        mat_contents = loadmat(file_name=array_path)
-
-        A = mat_contents["A"][0]
-        B = mat_contents["B"][0]
-        prev_obs = mat_contents["obs"].astype("int64")
-        policy = mat_contents["policy"].astype("int64") - 1
-        t_horizon = mat_contents["t_horizon"][0, 0].astype("int64")
-        prev_actions = mat_contents["previous_actions"].astype("int64") - 1
-        result_spm = mat_contents["qs"][0]
-        likelihoods = mat_contents["likelihoods"][0]
-
-        num_obs, num_states, _, num_factors = get_model_dimensions(A, B)
-        prev_obs = convert_observation_array(
-            prev_obs[:, max(0, curr_t - t_horizon) : (curr_t + 1)], num_obs
-        )
-
-        agent = Agent(A=A, B=B, C=C, inference_algo="MMP", policy_len=1, inference_horizon=t_horizon, use_BMA = False, policy_sep_prior = True)
-
         
 
     def test_active_inference_SPM_b(self):
--- test_scratch.py
+++ test_scratch.py
@@ -1,3 +1,4 @@
+# %%
 import os
 import unittest
 
@@ -6,9 +7,11 @@ from scipy.io import loadmat
 
 from pymdp.agent import Agent
 from pymdp.core.utils import random_A_matrix, random_B_matrix, obj_array_zeros, get_model_dimensions, convert_observation_array
+from pymdp.core.utils import to_arr_of_arr, to_numpy
 
 import matplotlib.pyplot as plt
 
+# %%
 DATA_PATH = "test/matlab_crossval/output/"
 
 """
@@ -20,7 +23,8 @@ mat_contents = loadmat(file_name=array_path)
 
 A = mat_contents["A"][0]
 B = mat_contents["B"][0]
-prev_obs = mat_contents["obs"].astype("int64")
+C = to_arr_of_arr(mat_contents["C"][0][0][:,0])
+obs_matlab = mat_contents["obs"].astype("int64")
 policy = mat_contents["policies"].astype("int64") - 1
 t_horizon = mat_contents["t_horizon"][0, 0].astype("int64")
 actions = mat_contents["actions"].astype("int64") - 1
@@ -28,8 +32,22 @@ qs = mat_contents["qs"][0]
 likelihoods = mat_contents["likelihoods"][0]
 
 num_obs, num_states, _, num_factors = get_model_dimensions(A, B)
-prev_obs = convert_observation_array(
-    prev_obs[:, max(0, curr_t - t_horizon) : (curr_t + 1)], num_obs
-)
+obs = convert_observation_array(obs_matlab, num_obs)
 
-agent = Agent(A=A, B=B, C=C, inference_algo="MMP", policy_len=1, inference_horizon=t_horizon, use_BMA = False, policy_sep_prior = True)
+agent = Agent(A=A, B=B, C=C, inference_algo="MMP", policy_len=1, 
+                inference_horizon=t_horizon, use_BMA = False, 
+                policy_sep_prior = True)
+
+T = len(obs)
+
+# %% Run loop over time
+for t in range(T):
+
+    o_t = (np.where(obs[t])[0][0],)
+    qx = agent.infer_states(o_t)
+    agent.infer_policies()
+    action = agent.sample_action()
+
+
+
+# %%
