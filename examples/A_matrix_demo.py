ea3e2577d369d923438d3c6f06c403975a3f8d2c
conorheins <conor.heins@gmail.com>
2021-07-10 12:30:38 +0200

    changed A matrix demos to accomodate new argument + other useability fixes

--- examples/A_matrix_demo.py
+++ examples/A_matrix_demo.py
@@ -13,7 +13,7 @@ module_path = str(path.parent) + '/'
 sys.path.append(module_path)
 
 import pymdp.core.utils as utils
-from pymdp.core.utils import create_A_matrix_stub
+from pymdp.core.utils import create_A_matrix_stub, read_A_matrix
 from pymdp.core.algos import run_fpi
 
 # %% Create an empty A matrix
@@ -35,50 +35,50 @@ model_labels = {
             },
         }
 
+num_obs, _, n_states, n_factors = utils.get_model_dimensions_from_labels(model_labels)
+
+read_from_excel = True
+pre_specified_excel = True
+
 A_stub = create_A_matrix_stub(model_labels)
 
-# %% Option 1: fill out A matrix 'offline' (e.g. in an excel spreadsheet)
+if read_from_excel:
+    ## Option 1: fill out A matrix 'offline' (e.g. in an excel spreadsheet)
 
-excel_dir = 'tmp_dir'
-if not os.path.exists(excel_dir):
-    os.mkdir(excel_dir)
+    excel_dir = 'tmp_dir'
+    if not os.path.exists(excel_dir):
+        os.mkdir(excel_dir)
 
-excel_path = os.path.join(excel_dir, 'my_a_matrix.xlsx')
+    excel_path = os.path.join(excel_dir, 'my_a_matrix.xlsx')
 
-A_stub.to_excel(excel_path)
+    if not pre_specified_excel:
+        A_stub.to_excel(excel_path)
+        print(f'Go fill out the A matrix in {excel_path} and then continue running this code\n')
+        
+if read_from_excel:
 
-# now go fill out the A matrix in the excel file you've written to disk
-# %% Now read it back in
-A_stub = read_A_matrix(excel_path)
+    A_stub = read_A_matrix(excel_path, n_factors)
 
-# %% Option 2: fill out the A matrix here in Python, using our knowledge of the dependencies in the system
+if not read_from_excel:
+    ## Option 2: fill out the A matrix here in Python, using our knowledge of the dependencies in the system and pandas multindexing assignments
 
-# A_stub.loc[('grass_observation','wet'),('rained', 'on')] = 1.0
+    A_stub.loc[('grass_observation','wet'),('rained', 'on')] = 1.0
 
-# A_stub.loc[('grass_observation','wet'),('rained', 'off')] = 0.7
-# A_stub.loc[('grass_observation','dry'),('rained', 'off')] = 0.3
+    A_stub.loc[('grass_observation','wet'),('rained', 'off')] = 0.7
+    A_stub.loc[('grass_observation','dry'),('rained', 'off')] = 0.3
 
-# A_stub.loc[('grass_observation','wet'),('did_not_rain', 'on')] = 0.5
-# A_stub.loc[('grass_observation','dry'),('did_not_rain', 'on')] = 0.5
+    A_stub.loc[('grass_observation','wet'),('did_not_rain', 'on')] = 0.5
+    A_stub.loc[('grass_observation','dry'),('did_not_rain', 'on')] = 0.5
 
-# A_stub.loc[('grass_observation','dry'),('did_not_rain', 'off')] = 1.0
+    A_stub.loc[('grass_observation','dry'),('did_not_rain', 'off')] = 1.0
 
-# A_stub.loc[('grass_observation','wet'),('rained', 'on')] = 1.0
+    A_stub.loc['weather_observation','rained'] = np.tile(np.array([0.1, 0.65, 0.25]).reshape(-1,1), (1,2)) 
 
-
-# A_stub.loc[('weather_observation','clear'),('rained')] = 0.2
-# A_stub.loc[('weather_observation','rainy'),('rained')] = 0.2
-# A_stub.loc[('weather_observation','cloudy'),('rained')] = 0.2
-
-# A_stub.loc['weather_observation','rained'] = np.tile(np.array([0.1, 0.65, 0.25]).reshape(-1,1), (1,2)) 
-
-# A_stub.loc[('weather_observation'),('did_not_rain')] = np.tile(np.array([0.9, 0.05, 0.05]).reshape(-1,1), (1,2)) 
+    A_stub.loc[('weather_observation'),('did_not_rain')] = np.tile(np.array([0.9, 0.05, 0.05]).reshape(-1,1), (1,2)) 
 
 # %% now convert the A matrix into a sequence of appopriately shaped numpy arrays
 
 A = utils.convert_A_stub_to_ndarray(A_stub, model_labels)
-
-num_obs, _, n_states, _ = utils.get_model_dimensions_from_labels(model_labels)
 
 obs_idx = [np.random.randint(o_dim) for o_dim in num_obs]
 
@@ -92,6 +92,3 @@ qs = run_fpi(A, observation, num_obs, n_states, prior=None, num_iter=10, dF=1.0,
 
 print('Belief that it rained: %.2f'%(qs[0][0]))
 print('Belief that the sprinkler was on: %.2f'%(qs[1][0]))
-
-
-# %%
