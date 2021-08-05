7d820eb2db5ea1a80acea3e7621d6317ae0dc529
conorheins <conor.heins@gmail.com>
2021-08-05 16:55:18 -0400

    added more unit-tests for agent initiailization for new refactor

--- test/test_agent_noCategorical.py
+++ test/test_agent_noCategorical.py
@@ -5,28 +5,97 @@ import numpy as np
 from scipy.io import loadmat
 
 from pymdp.agent import Agent, build_belief_array, build_xn_vn_array
-from pymdp.core.utils import random_A_matrix, random_B_matrix, obj_array_zeros, get_model_dimensions, convert_observation_array
-from pymdp.core.utils import to_arr_of_arr, to_numpy
+from pymdp.core import utils
 from pymdp.core import control
 
 class TestAgent_noCat(unittest.TestCase):
    
-    def test_agent_init(self):
+    def test_agent_init_with_control_fac_idx(self):
+        """
+        Initialize instance of the agent class and pass in a custom `control_fac_idx`
+        """
+
         num_obs = [2, 4]
         num_states = [2, 2]
-        num_control = [2, 2]
-        A = random_A_matrix(num_obs, num_states)
-        B = random_B_matrix(num_states, num_control)
+        num_controls = [2, 2]
+        A = utils.random_A_matrix(num_obs, num_states)
+        B = utils.random_B_matrix(num_states, num_controls)
 
-        C = obj_array_zeros([num_ob for num_ob in num_obs])
-        C[1][0] = 1.0  
-        C[1][1] = -2.0  
+        agent = Agent(A=A, B=B, control_fac_idx=[1])
 
-        agent = Agent(A=A, B=B, C=C, control_fac_idx=[1])
+        self.assertEqual(agent.num_controls[0], 1)
+        self.assertEqual(agent.num_controls[1], 2)
+    
+    def test_agent_init_without_control_fac_idx(self):
+        """
+        Initialize instance of the agent class and pass in a custom `control_fac_idx`
+        """
 
-        print(agent.num_controls)
+        num_obs = [2, 4]
+        num_states = [2, 2]
+        num_controls = [2, 2]
+        A = utils.random_A_matrix(num_obs, num_states)
+        B = utils.random_B_matrix(num_states, num_controls)
 
-        print(agent.policies)
+        agent = Agent(A=A, B=B)
+
+        self.assertEqual(agent.num_controls[0], 2)
+        self.assertEqual(agent.num_controls[1], 2)
+
+        self.assertEqual([0, 1], agent.control_fac_idx)
+
+    def test_reset_agent_VANILLA(self):
+        """
+        Ensure the `reset` method of Agent() using the new refactor is working as intended, 
+        using the `VANILLA` argument to `inference_algo` 
+        """
+
+        num_obs = [2, 4]
+        num_states = [2, 3]
+        num_controls = [2, 3]
+        A = utils.random_A_matrix(num_obs, num_states)
+        B = utils.random_B_matrix(num_states, num_controls)
+
+        agent = Agent(A=A, B=B, inference_algo = "VANILLA")
+
+        init_qs = utils.obj_array_uniform(agent.num_states)
+        self.assertTrue(all( [ (agent.qs[f] == init_qs[f]).all() for f in range(agent.num_factors)]) )
+        self.assertTrue(agent.curr_timestep == 0)
+
+    def test_reset_agent_MMP_wBMA(self):
+        """
+        Ensure the `reset` method of Agent() using the new refactor is working as intended, 
+        using the `MMP` argument to `inference_algo`, and `use_BMA` equal to True
+        """
+
+        num_obs = [2, 4]
+        num_states = [2, 3]
+        num_controls = [2, 3]
+        A = utils.random_A_matrix(num_obs, num_states)
+        B = utils.random_B_matrix(num_states, num_controls)
+
+        agent = Agent(A=A, B=B, inference_algo = "MMP", use_BMA=True)
+
+        self.assertEqual(len(agent.latest_belief), agent.num_factors)
+        self.assertTrue(all( [ (agent.latest_belief[f] == agent.D[f]).all() for f in range(agent.num_factors)]) )
+        self.assertTrue(agent.curr_timestep == 0)
+    
+    def test_reset_agent_MMP_wPSP(self):
+        """
+        Ensure the `reset` method of Agent() using the new refactor is working as intended, 
+        using the `MMP` argument to `inference_algo`, and `policy-separated prior` equal to True
+        """
+
+        num_obs = [2, 4]
+        num_states = [2, 3]
+        num_controls = [2, 3]
+        A = utils.random_A_matrix(num_obs, num_states)
+        B = utils.random_B_matrix(num_states, num_controls)
+
+        agent = Agent(A=A, B=B, inference_algo = "MMP", use_BMA = False, policy_sep_prior=True)
+
+        self.assertEqual(len(agent.qs[0]) - agent.inference_horizon - 1, agent.policy_len)
+
 
 if __name__ == "__main__":
     unittest.main()
