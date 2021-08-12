1873d777501ba8e2ab1ca25707a5fcb5f719a2ab
conorheins <conor.heins@gmail.com>
2021-08-12 18:00:43 -0400

    added unit tests for T-Maze w/ learning demo

--- test/test_demos.py
+++ test/test_demos.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 from pymdp.agent import Agent
 from pymdp.core import utils, maths, default_models
-from pymdp.envs import TMazeEnv
+from pymdp.envs import TMazeEnv, TMazeEnvNullOutcome
 
 class TestDemos(unittest.TestCase):
 
@@ -103,18 +103,13 @@ class TestDemos(unittest.TestCase):
         location_observations = ['CENTER','RIGHT ARM','LEFT ARM','CUE LOCATION']
         reward_observations = ['No reward','Reward!','Loss!']
         cue_observations = ['Cue Right','Cue Left']
-        msg = """ === Starting experiment === \n Reward condition: {}, Observation: [{}, {}, {}]"""
-        print(msg.format(reward_conditions[env.reward_condition], location_observations[obs[0]], reward_observations[obs[1]], cue_observations[obs[2]]))
-
+      
         for t in range(T):
             qx = agent.infer_states(obs)
 
             q_pi, efe = agent.infer_policies()
 
             action = agent.sample_action()
-
-            msg = """[Step {}] Action: [Move to {}]"""
-            print(msg.format(t, location_observations[int(action[0])]))
 
             obs = env.step(action)
 
@@ -128,10 +123,93 @@ class TestDemos(unittest.TestCase):
                 if env.reward_condition == 1:
                     self.assertEqual(obs[2], 1) # this tests that the cue observation is 'Cue Left' in case of 'Reward on Left' condition
 
-            msg = """[Step {}] Observation: [{},  {}, {}]"""
-            print(msg.format(t, location_observations[obs[0]], reward_observations[obs[1]], cue_observations[obs[2]]))
-
+            
         plot_beliefs(qx[1],"Final posterior beliefs about reward condition")
     
+    def test_tmaze_learning_demo(self):
+        """
+        This unit test runs the a concise version of the code in the `tmaze_demo_learning.ipynb` tutorial notebook to make sure it works if things are changed
+        """
+
+        def plot_beliefs(belief_dist, title=""):
+            plt.grid(zorder=0)
+            plt.bar(range(belief_dist.shape[0]), belief_dist, color='r', zorder=3)
+            plt.xticks(range(belief_dist.shape[0]))
+            plt.title(title)
+            
+        def plot_likelihood(A, title=""):
+            ax = sns.heatmap(A, cmap="OrRd", linewidth=2.5)
+            plt.xticks(range(A.shape[1]))
+            plt.yticks(range(A.shape[0]))
+            plt.title(title)
+        
+        reward_probabilities = [0.85, 0.15] # the 'true' reward probabilities 
+        env = TMazeEnvNullOutcome(reward_probs = reward_probabilities)
+        A_gp = env.get_likelihood_dist()
+        B_gp = env.get_transition_dist()
+
+        pA = utils.dirichlet_like(A_gp, scale = 1e16)
+
+        pA[1][1:,1:3,:] = 1.0
+
+        A_gm = utils.norm_dist_obj_arr(pA) 
+
+        B_gm = copy.deepcopy(B_gp)
+
+        controllable_indices = [0] # this is a list of the indices of the hidden state factors that are controllable
+        learnable_modalities = [1] # this is a list of the modalities that you want to be learn-able 
+
+        agent = Agent(A=A_gm,pA=pA,B=B_gm,
+              control_fac_idx=controllable_indices,
+              modalities_to_learn=learnable_modalities,
+              lr_pA = 0.25,
+              use_param_info_gain=True)
+
+        agent.D[0] = utils.onehot(0, agent.num_states[0])
+        agent.C[1][1] = 2.0
+        agent.C[1][2] = -2.0
+
+        T = 1000 # number of timesteps
+
+        obs = env.reset() # reset the environment and get an initial observation
+
+        for t in range(T):
+            agent.infer_states(obs)
+            agent.infer_policies()
+            action = agent.sample_action()
+            agent.update_A(obs)     
+            obs = env.step(action)
+        
+        # make sure they are learning the reward contingencies in the right general direction
+
+        REWARD_ON_RIGHT = 0
+        REWARD_ON_LEFT = 1
+
+        REWARD = 1
+        PUNISHMENT = 2
+
+        RIGHT_ARM = 1
+        LEFT_ARM = 2
+
+        # in case the reward condition is 'Reward on RIGHT' 
+
+        if env.reward_condition == REWARD_ON_RIGHT:
+
+            prob_reward_right = agent.A[1][REWARD,RIGHT_ARM,REWARD_ON_RIGHT]
+            prob_punishment_right = agent.A[1][PUNISHMENT,RIGHT_ARM,REWARD_ON_RIGHT]
+
+            self.assertGreater(prob_reward_right, prob_punishment_right)
+
+        # in case the reward condition is 'Reward on LEFT' 
+
+        elif env.reward_condition == REWARD_ON_LEFT:
+
+            prob_reward_left = agent.A[1][REWARD,LEFT_ARM,REWARD_ON_LEFT]
+            prob_punishment_left = agent.A[1][PUNISHMENT,LEFT_ARM,REWARD_ON_LEFT]
+            self.assertGreater(prob_reward_left, prob_punishment_left)
+        
+
+
+           
 if __name__ == "__main__":
     unittest.main()
