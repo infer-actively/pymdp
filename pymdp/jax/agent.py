fixed fpi error but now gradients true state inference do not work for num_iter > 2

pymdp/jax/agent.py
@@ -43,6 +43,7 @@ class Agent(Module):
     q_pi: Optional[List]
 
     # static parameters not leaves of the PyTree
+    num_iter: int = static_field()
     num_obs: List = static_field()
     num_modalities: int = static_field()
     num_states: List = static_field()
@@ -75,6 +76,7 @@ class Agent(Module):
         use_param_info_gain=False,
         action_selection="deterministic",
         inference_algo="VANILLA",
+        num_iter=16,
     ):
         ### PyTree leaves
 
@@ -92,6 +94,8 @@ class Agent(Module):
         self.gamma = jnp.broadcast_to(gamma, batch_dim) 
 
         ### Static parameters ###
+
+        self.num_iter = num_iter
 
         self.inference_algo = inference_algo
 
@@ -153,7 +157,8 @@ class Agent(Module):
         qs = inference.update_posterior_states(
             self.A,
             o_vec,
-            prior=empirical_prior
+            prior=empirical_prior,
+            num_iter=self.num_iter
         )
 
         return qs

pymdp/jax/algos.py
@@ -13,7 +13,8 @@ def marginal_log_likelihood(qs, log_likelihood, i):
 
     joint = log_likelihood * x
     dims = (f for f in range(len(qs)) if f != i)
-    return joint.sum(dims)/qs[i]
+    marg = joint.sum(dims)
+    return jnp.where(marg < 0., marg/qs[i], marg)
 
 def run_vanilla_fpi(A, obs, prior, num_iter=1):
     """ Vanilla fixed point iteration (jaxified) """

pymdp/jax/maths.py
@@ -1,8 +1,7 @@
 from jax import tree_util, nn, jit
 import jax.numpy as jnp
 
-MIN_VAL = 1e-16 # to debug weird inference with FPI, which we encountered with the T-Maze, try uncommenting this / commenting out the 1e-32 below
-# MIN_VAL = 1e-32
+MIN_VAL = jnp.finfo(float).eps
 
 def log_stable(x):
 