18052c9e58611253dba214836ab181aa96b26adf
conorheins <conor.heins@gmail.com>
2023-03-09 20:09:43 +0100

    made `A` matrix normalization error message for specific

--- pymdp/agent.py
+++ pymdp/agent.py
@@ -93,7 +93,7 @@ class Agent(object):
 
         self.A = utils.to_obj_array(A)
 
-        assert utils.is_normalized(self.A), "A matrix is not normalized (i.e. A.sum(axis = 0) must all equal 1.0)"
+        assert utils.is_normalized(self.A), "A matrix is not normalized (i.e. A[m].sum(axis = 0) must all equal 1.0 for all modalities)"
 
         # Determine number of observation modalities and their respective dimensions
         self.num_obs = [self.A[m].shape[0] for m in range(len(self.A))]
