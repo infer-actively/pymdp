d9bc4befad5cb207af2fb0e169c7ca34a5063c02
conorheins <conor.heins@gmail.com>
2023-08-01 19:22:52 +0200

    - added options to `update_posterior_states` to switch between MP algos
    Co-authored-by: Dimitrije Markovic <dimarkov@users.noreply.github.com>

--- pymdp/jax/inference.py
+++ pymdp/jax/inference.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=no-member
 
-from .algos import run_vanilla_fpi
+from .algos import run_vanilla_fpi, run_mmp, run_vmp, run_online_filtering
 
-def update_posterior_states(A, obs, prior=None, num_iter=16):
+def update_posterior_states(A, B, obs, actions, prior=None, A_dependencies=None, num_iter=16, method='fpi'):
 
-    return run_vanilla_fpi(A, obs, prior, num_iter=num_iter)
+    if method == 'fpi':
+        return run_vanilla_fpi(A, obs, prior, num_iter=num_iter)
+    if method == 'vmp':
+        return run_vmp(A, B, obs, prior, A_dependencies, num_iter=num_iter)
+    if method == 'mmp':
+        return run_mmp(A, B, obs, prior, A_dependencies, num_iter=num_iter)
+    if method == "ovf":
+        return run_online_filtering(A, B, obs, prior, A_dependencies, num_iter=num_iter)
    
