b018b8d0f1240d9e5e6b3ea0ba99e8f3ad466441
conorheins <conor.heins@gmail.com>
2023-09-21 18:08:31 +0200

    when method for inference == `fpi` or `ovf`, we append the history of beliefs to a growing list over time, to make it consistent with data structures used in VMP and MMP

--- pymdp/jax/inference.py
+++ pymdp/jax/inference.py
@@ -2,19 +2,15 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=no-member
 
-from .algos import run_vanilla_fpi, run_mmp, run_vmp, run_online_filtering
+from .algos import run_factorized_fpi, run_mmp, run_vmp
 
-def update_posterior_states(A, B, obs, prior=None, A_dependencies=None, num_iter=16, method='fpi'):
+def update_posterior_states(A, B, obs, prior=None, qs_hist=None, A_dependencies=None, num_iter=16, method='fpi'):
 
-    if method == 'fpi':
-        return run_vanilla_fpi(A, obs, prior, num_iter=num_iter)
+    if method == 'fpi' or method == "ovf":
+        qs = run_factorized_fpi(A, obs, prior, A_dependencies, num_iter=num_iter)
+        return qs_hist.append(qs)
     if method == 'vmp':
         return run_vmp(A, B, obs, prior, A_dependencies, num_iter=num_iter)
     if method == 'mmp':
         return run_mmp(A, B, obs, prior, A_dependencies, num_iter=num_iter)
-    # if method == "ovf":
-    #     prior, cond_prev = prior[0], prior[1]
-    #     qs, pred, cond = run_online_filtering(A, B, obs, prior, cond_prev, A_dependencies, num_iter=num_iter)
-    #     cond = [cond, cond_prev]
-    #     return qs, pred, cond
    
