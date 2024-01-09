20ba71b1dbb30fde6a6c77ec9ba96b5dd6d77fd5
Conor Heins <conor.heins@gmail.com>
2024-01-09 18:49:58 +0100

    inductive planning algorithm now in jax - see `inductive_inference_example.ipynb` for application in random generative model

--- pymdp/jax/control.py
+++ pymdp/jax/control.py
@@ -6,11 +6,12 @@
 import itertools
 import jax.numpy as jnp
 import jax.tree_util as jtu
-from typing import Tuple, Optional
+from typing import List, Tuple, Optional
 from functools import partial
 from jax import lax, jit, vmap, nn
 from jax import random as jr
 from itertools import chain
+from jaxtyping import Array
 
 from pymdp.jax.maths import *
 # import pymdp.jax.utils as utils
@@ -337,6 +338,152 @@ def compute_G_policy(qs_init, A, B, C, pA, pB, A_dependencies, B_dependencies, p
     qs_final, neg_G = final_state
     return neg_G
 
+def compute_G_policy_inductive(qs_init, A, B, C, A_dependencies, B_dependencies, I, policy_i, use_utility=True, use_inductive=True, inductive_epsilon=1e-3):
+    """ 
+    Write a version of compute_G_policy that does the same computations as `compute_G_policy` but using `lax.scan` instead of a for loop.
+    @NOTE: THis one further includes computations used for inductive planning, which only works in case of deterministic generative models so info gain is excluded (for now)
+    """
+
+    def scan_body(carry, t):
+
+        qs, neg_G = carry
+
+        qs_next = compute_expected_state(qs, B, policy_i[t], B_dependencies)
+
+        qo = compute_expected_obs(qs_next, A, A_dependencies)
+
+        # info_gain = compute_info_gain(qs_next, qo, A, A_dependencies) if use_states_info_gain else 0.
+
+        utility = compute_expected_utility(qo, C) if use_utility else 0.
+
+        inductive_value = calc_inductive_value_t(qs_init, qs_next, I, epsilon=inductive_epsilon) if use_inductive else 0.
+
+        neg_G = utility + inductive_value
+
+        # param_info_gain = calc_pA_info_gain(pA, qo, qs_next) if use_param_info_gain else 0.
+        # param_info_gain += calc_pB_info_gain(pB, qs_next, qs) if use_param_info_gain else 0.
+
+        # neg_G += info_gain + utility + param_info_gain
+
+        return (qs_next, neg_G), None
+
+    qs = qs_init
+    neg_G = 0.
+    final_state, _ = lax.scan(scan_body, (qs, neg_G), jnp.arange(policy_i.shape[0]))
+    qs_final, neg_G = final_state
+    return neg_G
+
+def update_posterior_policies_inductive(policy_matrix, qs_init, A, B, C, A_dependencies, B_dependencies, I, gamma=16.0, use_utility=True, use_inductive=True, inductive_epsilon=1e-3):
+    # policy --> n_levels_factor_f x 1
+    # factor --> n_levels_factor_f x n_policies
+    ## vmap across policies
+    compute_G_fixed_states = partial(compute_G_policy_inductive, qs_init, A, B, C, A_dependencies, B_dependencies, I,
+                                     use_utility=use_utility, use_inductive=use_inductive, inductive_epsilon=inductive_epsilon)
+
+    # only in the case of policy-dependent qs_inits
+    # in_axes_list = (1,) * n_factors
+    # all_efe_of_policies = vmap(compute_G_policy, in_axes=(in_axes_list, 0))(qs_init_pi, policy_matrix)
+
+    # policies needs to be an NDarray of shape (n_policies, n_timepoints, n_control_factors)
+    neg_efe_all_policies = vmap(compute_G_fixed_states)(policy_matrix)
+
+    return nn.softmax(gamma * neg_efe_all_policies), neg_efe_all_policies
+
+def generate_I_matrix(H: List[Array], B: List[Array], threshold: float, depth: int):
+    """ 
+    Generates the `I` matrices used in inductive planning. These matrices stores the probability of reaching the goal state backwards from state j (columns) after i (rows) steps.
+    Parameters
+    ----------    
+    H: ``list`` of ``jax.numpy.ndarray``
+        Constraints over desired states (1 if you want to reach that state, 0 otherwise)
+    B: ``list`` of ``jax.numpy.ndarray``
+        Dynamics likelihood mapping or 'transition model', mapping from hidden states at ``t`` to hidden states at ``t+1``, given some control state ``u``.
+        Each element ``B[f]`` of this object array stores a 3-D tensor for hidden state factor ``f``, whose entries ``B[f][s, v, u]`` store the probability
+        of hidden state level ``s`` at the current time, given hidden state level ``v`` and action ``u`` at the previous time.
+    threshold: ``float``
+        The threshold for pruning transitions that are below a certain probability
+    depth: ``int``
+        The temporal depth of the backward induction
+
+    Returns
+    ----------
+    I: ``numpy.ndarray`` of dtype object
+        For each state factor, contains a 2D ``numpy.ndarray`` whose element i,j yields the probability 
+        of reaching the goal state backwards from state j after i steps.
+    """
+    
+    num_factors = len(H)
+    I = []
+    for f in range(num_factors):
+        """
+        For each factor, we need to compute the probability of reaching the goal state
+        """
+
+        # If there exists an action that allows transitioning 
+        # from state to next_state, with probability larger than threshold
+        # set b_reachable[current_state, previous_state] to 1
+        b_reachable = jnp.where(B[f] > threshold, 1.0, 0.0).sum(axis=-1)
+        b_reachable = jnp.where(b_reachable > 0., 1.0, 0.0)
+
+        def step_fn(carry, i):
+            I_prev = carry
+            I_next = jnp.dot(b_reachable, I_prev)
+            I_next = jnp.where(I_next > 0.1, 1.0, 0.0) # clamp I_next to 1.0 if it's above 0.1, 0 otherwise
+            return I_next, I_next
+    
+        _, I_f = lax.scan(step_fn, H[f], jnp.arange(depth-1))
+        I_f = jnp.concatenate([H[f][None,...], I_f], axis=0)
+
+        I.append(I_f)
+    
+    return I
+
+def calc_inductive_value_t(qs, qs_next, I, epsilon=1e-3):
+    """
+    Computes the inductive value of a state at a particular time (translation of @tverbele's `numpy` implementation of inductive planning, formerly
+    called `calc_inductive_cost`).
+
+    Parameters
+    ----------
+    qs: ``list`` of ``jax.numpy.ndarray`` 
+        Marginal posterior beliefs over hidden states at a given timepoint.
+    qs_next: ```list`` of ``jax.numpy.ndarray`` 
+        Predictive posterior beliefs over hidden states expected under the policy.
+    I: ``numpy.ndarray`` of dtype object
+        For each state factor, contains a 2D ``numpy.ndarray`` whose element i,j yields the probability 
+        of reaching the goal state backwards from state j after i steps.
+
+    Returns
+    -------
+    inductive_val: float
+        Value (negative inductive cost) of visiting this state using backwards induction under the policy in question
+    """
+    
+    # initialise inductive value
+    inductive_val = 0.
+
+    log_eps = log_stable(epsilon)
+    for f in range(len(qs)):
+        # we also assume precise beliefs here?!
+        idx = jnp.argmax(qs[f])
+        # m = arg max_n p_n < sup p
+
+        # i.e. find first entry at which I_idx equals 1, and then m is the index before that
+        m = jnp.maximum(jnp.argmax(I[f][:, idx])-1, 0)
+        I_m = (1.-I[f][m, :]) * log_eps
+        path_available = jnp.clip(I[f][:,idx].sum(0), a_min=0, a_max=1) # if there are any 1's at all in that column of I, then this == 1, otherwise 0
+        inductive_val += path_available * I_m.dot(qs_next[f]) # scaling by path_available will nullify the addition of inductive value in the case we find no path to goal (i.e. when no goal specified)
+
+        # The below is the straight translation from numpy, but unfortunately doesn't work due to non-statically sized arrays (AKA the call to jnp.where)
+        # # i.e. find first I idx equals 1 and m is the index before
+        # m = jnp.where(I[f][:, idx] == 1)[0]
+        # # we might find no path to goal (i.e. when no goal specified)
+        # if len(m) > 0:
+        #     m = jnp.maximum(m[0]-1, 0)
+        #     I_m = (1.-I[f][m, :]) * log_eps
+        #     inductive_val += I_m.dot(qs_next[f])
+                
+    return inductive_val
 
 # if __name__ == '__main__':
 
