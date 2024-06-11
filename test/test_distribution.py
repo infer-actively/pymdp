0c63c362ea890510c1ab8846db2751eeb7f3f7c9
Ozan Catal <ozan.catal@verses.ai>
2024-06-11 15:55:19 +0200

    acknowledge that the alphabet has an ordering

--- test/test_distribution.py
+++ test/test_distribution.py
@@ -99,7 +99,7 @@ class TestDists(unittest.TestCase):
                 },
             },
         }
-        trans, like = distribution.compile_model(model_example)
+        like, trans = distribution.compile_model(model_example)
         self.assertEqual(len(trans), 2)
         self.assertEqual(len(like), 2)
         self.assertEqual(trans[0].data.shape, (3, 3, 2, 2, 2))
