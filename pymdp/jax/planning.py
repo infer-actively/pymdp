f9c11a4ec83694fd94d63101a1bdae4c88b9fa76
Alexander Tschantz <tschantz.alec@gmail.com>
2024-06-12 12:20:16 +0200

    step function

--- pymdp/jax/planning.py
+++ pymdp/jax/planning.py
@@ -3,8 +3,7 @@ import jax.numpy as jnp
 import jax.tree_util as jtu
 from jax import nn
 
-
-from pymdp.jax.control import compute_info_gain, compute_expected_utility
+from pymdp.jax.control import compute_info_gain, compute_expected_utility, compute_expected_state, compute_expected_obs
 
 
 class Tree:
@@ -126,3 +125,25 @@ def expand_node(
                 }
                 policy_node["children"].append(observation_node)
                 tree.append(observation_node)
+
+
+def step(agent, qs, policies, gamma=32):
+    qs_pis, qo_pis, utilities, info_gains, G = [], [], [], [], []
+
+    def _step(a, b, c, q, policy):
+        qs_pi = compute_expected_state(q, b, policy, agent.B_dependencies)
+        qo_pi = compute_expected_obs(qs_pi, a, agent.A_dependencies)
+        utility = compute_expected_utility(qo_pi, c)
+        info_gain = compute_info_gain(qs_pi, qo_pi, a, agent.A_dependencies)
+        return qs_pi, qo_pi, utility, info_gain
+
+    for policy in policies:
+        qs_pi, qo_pi, utility, info_gain = jax.vmap(_step)(agent.A, agent.B, agent.C, qs, policy)
+        qs_pis.append(qs_pi)
+        qo_pis.append(qo_pi)
+        utilities.append(utility)
+        info_gains.append(info_gain)
+        G.append(-utility - info_gain)
+
+    G = nn.softmax(-gamma * jnp.array(G), axis=0)
+    return qs_pis, qo_pis, utilities, info_gains, G
