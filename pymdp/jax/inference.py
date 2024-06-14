59131c6c1860ed7b5d4893b13e80682e4a7a0881
Toon Van de Maele <toonvandemaele@gmail.com>
2024-06-14 11:15:37 +0200

    Unit test for ovf smoothing using sparse tensors

--- pymdp/jax/inference.py
+++ pymdp/jax/inference.py
@@ -9,21 +9,33 @@ from multimethod import multimethod
 from jax.experimental.sparse._base import JAXSparse
 from jax.experimental.sparse import sparsify
 from jaxtyping import Array
+import copy
+
+
+def to_dense(x: JAXSparse):
+    """
+    Create a dense array from a jax sparse array in case the _validate for normal densify fails
+    """
+    dense = jnp.zeros(x.shape)
+    for val, idcs in zip(x.data.reshape(-1), x.indices.reshape(-1, len(x.shape))):
+        dense = dense.at[*idcs].set(val)
+    return dense
+
 
 def update_posterior_states(
-        A, 
-        B, 
-        obs, 
-        past_actions, 
-        prior=None, 
-        qs_hist=None, 
-        A_dependencies=None, 
-        B_dependencies=None, 
-        num_iter=16, 
-        method='fpi'
-    ):
+    A,
+    B,
+    obs,
+    past_actions,
+    prior=None,
+    qs_hist=None,
+    A_dependencies=None,
+    B_dependencies=None,
+    num_iter=16,
+    method="fpi",
+):
 
-    if method == 'fpi' or method == "ovf":
+    if method == "fpi" or method == "ovf":
         # format obs to select only last observation
         curr_obs = jtu.tree_map(lambda x: x[-1], obs)
         qs = run_factorized_fpi(A, curr_obs, prior, A_dependencies, num_iter=num_iter)
@@ -33,7 +45,7 @@ def update_posterior_states(
         if past_actions is not None:
             nf = len(B)
             actions_tree = [past_actions[:, i] for i in range(nf)]
-            
+
             # move time steps to the leading axis (leftmost)
             # this assumes that a policy is always specified as the rightmost axis of Bs
             B = jtu.tree_map(lambda b, a_idx: jnp.moveaxis(b[..., a_idx], -1, 0), B, actions_tree)
@@ -41,24 +53,25 @@ def update_posterior_states(
             B = None
 
         # outputs of both VMP and MMP should be a list of hidden state factors, where each qs[f].shape = (T, batch_dim, num_states_f)
-        if method == 'vmp':
-            qs = run_vmp(A, B, obs, prior, A_dependencies, B_dependencies, num_iter=num_iter) 
-        if method == 'mmp':
+        if method == "vmp":
+            qs = run_vmp(A, B, obs, prior, A_dependencies, B_dependencies, num_iter=num_iter)
+        if method == "mmp":
             qs = run_mmp(A, B, obs, prior, A_dependencies, B_dependencies, num_iter=num_iter)
-    
+
     if qs_hist is not None:
-        if method == 'fpi' or method == "ovf":
+        if method == "fpi" or method == "ovf":
             qs_hist = jtu.tree_map(lambda x, y: jnp.concatenate([x, jnp.expand_dims(y, 0)], 0), qs_hist, qs)
         else:
-            #TODO: return entire history of beliefs
+            # TODO: return entire history of beliefs
             qs_hist = qs
     else:
-        if method == 'fpi' or method == "ovf":
+        if method == "fpi" or method == "ovf":
             qs_hist = jtu.tree_map(lambda x: jnp.expand_dims(x, 0), qs)
         else:
             qs_hist = qs
-    
+
     return qs_hist
+
 
 @multimethod
 def joint_dist_factor(b: Array, filtered_qs, actions):
@@ -73,30 +86,23 @@ def joint_dist_factor(b: Array, filtered_qs, actions):
     qs_joint = time_b * jnp.expand_dims(qs_filter, -1)
 
     # cond dist - timestep x s_{t} | s_{t+1}
-    qs_backward_cond = jnp.moveaxis(
-        qs_joint / qs_joint.sum(-2, keepdims=True), -2, -1
-    )
+    qs_backward_cond = jnp.moveaxis(qs_joint / qs_joint.sum(-2, keepdims=True), -2, -1)
     # tranpose_idx = list(range(len(qs_joint.shape[:-2]))) + [qs_joint.ndim-1, qs_joint.ndim-2]
     # qs_backward_cond = (qs_joint / qs_joint.sum(-2, keepdims=True).todense()).transpose(tranpose_idx)
 
     def step_fn(qs_smooth_past, backward_b):
         qs_joint = backward_b * qs_smooth_past
         qs_smooth = qs_joint.sum(-1)
-        
+
         return qs_smooth, (qs_smooth, qs_joint)
 
     # seq_qs will contain a sequence of smoothed marginals and joints
-    _, seq_qs = lax.scan(
-        step_fn,
-        qs_last,
-        qs_backward_cond,
-        reverse=True,
-        unroll=2
-    )
+    _, seq_qs = lax.scan(step_fn, qs_last, qs_backward_cond, reverse=True, unroll=2)
 
     # we add the last filtered belief to smoothed beliefs
     qs_smooth_all = jnp.concatenate([seq_qs[0], jnp.expand_dims(qs_last, 0)], 0)
     return qs_smooth_all, seq_qs[1]
+
 
 @multimethod
 def joint_dist_factor(b: JAXSparse, filtered_qs, actions):
@@ -104,32 +110,25 @@ def joint_dist_factor(b: JAXSparse, filtered_qs, actions):
     qs_filter = filtered_qs[:-1]
 
     # conditional dist - timestep x s_{t+1} | s_{t}
-    time_b = b[...,actions].transpose([b.ndim-1] + list(range(b.ndim-1)))
+    time_b = b[..., actions].transpose([b.ndim - 1] + list(range(b.ndim - 1)))
 
     # joint dist - timestep x s_{t+1} x s_{t}
     qs_joint = time_b * jnp.expand_dims(qs_filter, -1)
 
     # cond dist - timestep x s_{t} | s_{t+1}
-    tranpose_idx = list(range(len(qs_joint.shape[:-2]))) + [qs_joint.ndim-1, qs_joint.ndim-2]
+    tranpose_idx = list(range(len(qs_joint.shape[:-2]))) + [qs_joint.ndim - 1, qs_joint.ndim - 2]
     qs_backward_cond = (qs_joint / qs_joint.sum(-2, keepdims=True).todense()).transpose(tranpose_idx)
 
     def step_fn(qs_smooth_past, t):
         qs_joint = qs_backward_cond[t] * qs_smooth_past
         qs_smooth = qs_joint.sum(-1)
-        
-        return qs_smooth.todense(), (qs_smooth.todense(), qs_joint)
+
+        return qs_smooth.todense(), (qs_smooth.todense(), to_dense(qs_joint))
 
     # seq_qs will contain a sequence of smoothed marginals and joints
-    _, seq_qs = lax.scan(
-        step_fn,
-        qs_last,
-        jnp.arange(qs_backward_cond.shape[0]),
-        reverse=True,
-        unroll=2
-    )
+    _, seq_qs = lax.scan(step_fn, qs_last, jnp.arange(qs_backward_cond.shape[0]), reverse=True, unroll=2)
 
     # we add the last filtered belief to smoothed beliefs
-
     qs_smooth_all = jnp.concatenate([seq_qs[0], jnp.expand_dims(qs_last, 0)], 0)
     return qs_smooth_all, seq_qs[1]
 
@@ -147,6 +146,3 @@ def smoothing_ovf(filtered_post, B, past_actions):
         marginals_and_joints.append(marginals_and_joints_f)
 
     return marginals_and_joints
-
-
-    
--- test/test_jax_sparse_backend.py
+++ test/test_jax_sparse_backend.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """ Unit Tests
-__author__: Conor Heins, Toon van der Maele, Ozan Catal
+__author__: Conor Heins, Toon Van de Maele, Ozan Catal
 """
 
 import os
@@ -17,115 +17,152 @@ from jax import random as jr
 
 from pymdp.jax.inference import smoothing_ovf
 from pymdp import utils, maths
+from pymdp.control import construct_policies
+
+from jax.experimental import sparse
 
 from typing import Any, List, Dict
+
 
 def make_model_configs(source_seed=0, num_models=4) -> Dict:
     """
     This creates a bunch of model configurations (random amounts of num states, num obs, num controls, etc.)
-    that will be looped over and used as inputs for each unit test. This is intended to test each function on a variety of 
-    differently-dimensioned generative models 
+    that will be looped over and used as inputs for each unit test. This is intended to test each function on a variety of
+    differently-dimensioned generative models
     """
     ""
     rng_keys = jr.split(jr.PRNGKey(source_seed), num_models)
-    num_factors_list = [ jr.randint(key, (1,), 1, 7)[0].item() for key in rng_keys ] # list of total numbers of hidden state factors per model
-    num_states_list = [ jr.randint(key, (nf,), 2, 5).tolist() for nf, key in zip(num_factors_list, rng_keys) ]
-    num_controls_list = [ jr.randint(key, (nf,), 1, 3).tolist() for nf, key in zip(num_factors_list, rng_keys) ]
+    num_factors_list = [
+        jr.randint(key, (1,), 1, 7)[0].item() for key in rng_keys
+    ]  # list of total numbers of hidden state factors per model
+    num_states_list = [jr.randint(key, (nf,), 2, 5).tolist() for nf, key in zip(num_factors_list, rng_keys)]
+    num_controls_list = [jr.randint(key, (nf,), 1, 3).tolist() for nf, key in zip(num_factors_list, rng_keys)]
 
     rng_keys = jr.split(rng_keys[-1], num_models)
-    num_modalities_list = [ jr.randint(key, (1,), 1, 10)[0].item() for key in rng_keys ]
-    num_obs_list = [ jr.randint(key, (nm,), 1, 5).tolist() for nm, key in zip(num_modalities_list, rng_keys) ]
+    num_modalities_list = [jr.randint(key, (1,), 1, 10)[0].item() for key in rng_keys]
+    num_obs_list = [jr.randint(key, (nm,), 1, 5).tolist() for nm, key in zip(num_modalities_list, rng_keys)]
 
     rng_keys = jr.split(rng_keys[-1], num_models)
     A_deps_list, B_deps_list = [], []
     for nf, nm, model_key in zip(num_factors_list, num_modalities_list, rng_keys):
         modality_keys_model_i = jr.split(model_key, nm)
-        num_f_per_modality = [jr.randint(key, shape=(), minval=1, maxval=nf+1).item() for key in modality_keys_model_i] # this is the number of factors that each modality depends on
-        A_deps_model_i = [sorted(jr.choice(key, a=nf, shape=(num_f_m,), replace=False).tolist()) for key, num_f_m in zip(modality_keys_model_i, num_f_per_modality)]
+        num_f_per_modality = [
+            jr.randint(key, shape=(), minval=1, maxval=nf + 1).item() for key in modality_keys_model_i
+        ]  # this is the number of factors that each modality depends on
+        A_deps_model_i = [
+            sorted(jr.choice(key, a=nf, shape=(num_f_m,), replace=False).tolist())
+            for key, num_f_m in zip(modality_keys_model_i, num_f_per_modality)
+        ]
         A_deps_list.append(A_deps_model_i)
 
         factor_keys_model_i = jr.split(modality_keys_model_i[-1], nf)
-        num_f_per_factor = [jr.randint(key, shape=(), minval=1, maxval=nf+1).item() for key in factor_keys_model_i] # this is the number of factors that each factor depends on
-        B_deps_model_i = [sorted(jr.choice(key, a=nf, shape=(num_f_f,), replace=False).tolist()) for key, num_f_f in zip(factor_keys_model_i, num_f_per_factor)]
+        num_f_per_factor = [
+            jr.randint(key, shape=(), minval=1, maxval=nf + 1).item() for key in factor_keys_model_i
+        ]  # this is the number of factors that each factor depends on
+        B_deps_model_i = [
+            sorted(jr.choice(key, a=nf, shape=(num_f_f,), replace=False).tolist())
+            for key, num_f_f in zip(factor_keys_model_i, num_f_per_factor)
+        ]
         B_deps_list.append(B_deps_model_i)
 
-    return {'nf_list': num_factors_list, 
-            'ns_list': num_states_list, 
-            'nc_list': num_controls_list,
-            'nm_list': num_modalities_list, 
-            'no_list': num_obs_list, 
-            'A_deps_list': A_deps_list,
-            'B_deps_list': B_deps_list
-        }
+    return {
+        "nf_list": num_factors_list,
+        "ns_list": num_states_list,
+        "nc_list": num_controls_list,
+        "nm_list": num_modalities_list,
+        "no_list": num_obs_list,
+        "A_deps_list": A_deps_list,
+        "B_deps_list": B_deps_list,
+    }
 
-def make_A_full(A_reduced: List[np.ndarray], A_dependencies: List[List[int]], num_obs: List[int], num_states: List[int]) -> np.ndarray:
-    """ 
+
+def make_A_full(
+    A_reduced: List[np.ndarray], A_dependencies: List[List[int]], num_obs: List[int], num_states: List[int]
+) -> np.ndarray:
+    """
     Given a reduced A matrix, `A_reduced`, and a list of dependencies between hidden state factors and observation modalities, `A_dependencies`,
     return a full A matrix, `A_full`, where `A_full[m]` is the full A matrix for modality `m`. This means all redundant conditional independencies
     between observation modalities `m` and all hidden state factors (i.e. `range(len(num_states))`) are represented as lagging dimensions in `A_full`.
     """
-    A_full = utils.initialize_empty_A(num_obs, num_states) # initialize the full likelihood tensor (ALL modalities might depend on ALL factors)
-    all_factors = range(len(num_states)) # indices of all hidden state factors
+    A_full = utils.initialize_empty_A(
+        num_obs, num_states
+    )  # initialize the full likelihood tensor (ALL modalities might depend on ALL factors)
+    all_factors = range(len(num_states))  # indices of all hidden state factors
     for m, A_m in enumerate(A_full):
 
         # Step 1. Extract the list of the factors that modality `m` does NOT depend on
-        non_dependent_factors = list(set(all_factors) - set(A_dependencies[m])) 
+        non_dependent_factors = list(set(all_factors) - set(A_dependencies[m]))
 
-        # Step 2. broadcast or tile the reduced A matrix (`A_reduced`) along the dimensions of corresponding to `non_dependent_factors`, to give it the full shape of `(num_obs[m], *num_states)`
+        # Step 2. broadcast or tile the reduced A matrix (`A_reduced`) along the dimensions of corresponding to
+        # `non_dependent_factors`, to give it the full shape of `(num_obs[m], *num_states)`
         expanded_dims = [num_obs[m]] + [1 if f in non_dependent_factors else ns for (f, ns) in enumerate(num_states)]
         tile_dims = [1] + [ns if f in non_dependent_factors else 1 for (f, ns) in enumerate(num_states)]
         A_full[m] = np.tile(A_reduced[m].reshape(expanded_dims), tile_dims)
-    
+
     return A_full
-                
+
+
 class TestJaxSparseOperations(unittest.TestCase):
 
     def test_sparse_smoothing(self):
-        cfg = {'source_seed': 0,
-                'num_models': 4
-            }
+        cfg = {"source_seed": 1, "num_models": 4}
         gm_params = make_model_configs(**cfg)
-        num_states_list, num_obs_list = gm_params['ns_list'], gm_params['no_list']
+        num_states_list, num_obs_list = gm_params["ns_list"], gm_params["no_list"]
+        num_controls_list, B_deps_list = gm_params["nc_list"], gm_params["B_deps_list"]
 
-        for (num_states, num_obs) in zip(num_states_list, num_obs_list):
+        num_states_list = num_states_list
 
-            # Make numpy versions of each generative model component and observatiosn
-            prior = utils.random_single_categorical(num_states)
-            A = utils.random_A_matrix(num_obs, num_states)
+        n_time = 8
+        n_batch = 1
 
-            obs = utils.obj_array(len(num_obs))
-            for m, obs_dim in enumerate(num_obs):
-                obs[m] = utils.onehot(np.random.randint(obs_dim), obs_dim)
+        for num_states, num_obs, num_controls in zip(num_states_list, num_obs_list, num_controls_list):
 
-            # extract B's, D',s etc.
+            # Randomly create a B matrix that contains a lot of zeros
+            B = utils.random_B_matrix(num_states, num_controls)
+            B = [jnp.array(x.astype(np.float32)) for x in B]
+            # Map all values below the mean to 0 to create a B tensor with zeros
+            B = jtu.tree_map(lambda x: jnp.array(utils.norm_dist(jnp.clip((x - x.mean()), 0, 1))), B)
 
-            # dense jax version
-            prior = [jnp.array(prior_f) for prior_f in prior]
-            A = [jnp.array(a_m) for a_m in A]
-            obs = [jnp.array(o_m) for o_m in obs]
-            # ... finish making generative model
-            # .... put the dense version of smoothing_ovf here
+            # Create a sparse array B
+            sparse_B = jtu.tree_map(lambda b: sparse.BCOO.fromdense(b), B)
 
+            # Construct a random list of actions
+            policies = construct_policies(num_states, num_controls, policy_len=1)
+            acs = [None for _ in range(n_time - 1)]
+            for t in range(n_time - 1):
+                pol = policies[np.random.randint(len(policies))]
+                # Get rid of the policy length index, and insert batch dim
+                pol = jnp.expand_dims(pol[0], 0)
+                # Broadcast to add in the batch dim
+                pol = jnp.broadcast_to(pol, (n_batch, 1, len(num_controls)))
+                acs[t] = pol
+            action_hist = jnp.concatenate(acs, axis=1)
 
-            # sparse jax version
-            prior = [jnp.array(prior_f) for prior_f in prior]
-            A = [jnp.array(a_m) for a_m in A]
-            obs = [jnp.array(o_m) for o_m in obs]
-            # ... finish making generative model
-            # .... put the sparse version of smoothing_ovf here
+            # Construct a random list of beliefs
+            beliefs = [None for _ in range(len(num_states))]
+            for m, ns in enumerate(num_states):
+                beliefs[m] = np.random.uniform(0, 1, size=(n_batch, n_time, ns))
+                beliefs[m] /= beliefs[m].sum(axis=-1, keepdims=True)
+                beliefs[m] = jnp.array(beliefs[m])
 
-            # for example, something like this
-            for f, (dense_out, sparse_out) in enumerate(zip(smoothed_beliefs_dense, smoothed_beliefs_sparse)):
-                self.assertTrue(np.allclose(dense_out[f], sparse_out[f]))
+            # Take the ith element from the pytree (not testing batched here)
+            take_i = lambda pytree, i: jtu.tree_map(lambda leaf: leaf[i], pytree)
 
+            for i in range(n_batch):
+                smoothed_beliefs_dense = smoothing_ovf(take_i(beliefs, i), B, action_hist[i])
+
+                # sparse jax version
+                smoothed_beliefs_sparse = smoothing_ovf(take_i(beliefs, i), sparse_B, action_hist[i])
+
+                # for example, something like this
+                for f, (dense_out, sparse_out) in enumerate(zip(smoothed_beliefs_dense, smoothed_beliefs_sparse)):
+
+                    qs_smooth_dense, qs_joint_dense = dense_out
+                    qs_smooth_sparse, qs_joint_sparse = sparse_out
+
+                    self.assertTrue(np.allclose(qs_smooth_dense, qs_smooth_sparse))
+                    self.assertTrue(np.allclose(qs_joint_dense, qs_joint_sparse))
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
-
-    
