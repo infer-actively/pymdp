0c05b54390ae45b0622d83ca9c9ac4d63dd59a9e
dimarkov <5038100+dimarkov@users.noreply.github.com>
2024-06-17 14:04:51 +0200

    implementation of backward smoothing with dense and sparse tensors

--- pymdp/jax/inference.py
+++ pymdp/jax/inference.py
@@ -4,7 +4,10 @@
 
 import jax.numpy as jnp
 from .algos import run_factorized_fpi, run_mmp, run_vmp
-from jax import tree_util as jtu
+from jax import tree_util as jtu, lax
+from multimethod import multimethod
+from jax.experimental.sparse._base import JAXSparse
+from jaxtyping import Array
 
 def update_posterior_states(
         A, 
@@ -55,4 +58,94 @@ def update_posterior_states(
             qs_hist = qs
     
     return qs_hist
+
+@multimethod
+def joint_dist_factor(b: Array, filtered_qs, actions):
+    qs_last = filtered_qs[-1]
+    qs_filter = filtered_qs[:-1]
+
+    # conditional dist - timestep x s_{t+1} | s_{t}
+    time_b = jnp.moveaxis(b[..., actions], -1, 0)
+    # time_b = b[...,actions].transpose([b.ndim-1] + list(range(b.ndim-1)))
+
+    # joint dist - timestep x s_{t+1} x s_{t}
+    qs_joint = time_b * jnp.expand_dims(qs_filter, -1)
+
+    # cond dist - timestep x s_{t} | s_{t+1}
+    qs_backward_cond = jnp.moveaxis(
+        qs_joint / qs_joint.sum(-2, keepdims=True), -2, -1
+    )
+    # tranpose_idx = list(range(len(qs_joint.shape[:-2]))) + [qs_joint.ndim-1, qs_joint.ndim-2]
+    # qs_backward_cond = (qs_joint / qs_joint.sum(-2, keepdims=True).todense()).transpose(tranpose_idx)
+
+    def step_fn(qs_smooth_past, backward_b):
+        qs_joint = backward_b * qs_smooth_past
+        qs_smooth = qs_joint.sum(-1)
+        
+        return qs_smooth, (qs_smooth, qs_joint)
+
+    # seq_qs will contain a sequence of smoothed marginals and joints
+    _, seq_qs = lax.scan(
+        step_fn,
+        qs_last,
+        qs_backward_cond,
+        reverse=True,
+        unroll=2
+    )
+
+    # we add the last filtered belief to smoothed beliefs
+    qs_smooth_all = jnp.concatenate([seq_qs[0], jnp.expand_dims(qs_last, 0)], 0)
+    return qs_smooth_all, seq_qs[1]
+
+@multimethod
+def joint_dist_factor(b: JAXSparse, filtered_qs, actions):
+    qs_last = filtered_qs[-1]
+    qs_filter = filtered_qs[:-1]
+
+    # conditional dist - timestep x s_{t+1} | s_{t}
+    time_b = b[...,actions].transpose([b.ndim-1] + list(range(b.ndim-1)))
+
+    # joint dist - timestep x s_{t+1} x s_{t}
+    qs_joint = time_b * jnp.expand_dims(qs_filter, -1)
+
+    # cond dist - timestep x s_{t} | s_{t+1}
+    tranpose_idx = list(range(len(qs_joint.shape[:-2]))) + [qs_joint.ndim-1, qs_joint.ndim-2]
+    qs_backward_cond = (qs_joint / qs_joint.sum(-2, keepdims=True).todense()).transpose(tranpose_idx)
+
+    def step_fn(qs_smooth_past, t):
+        qs_joint = qs_backward_cond[t] * qs_smooth_past
+        qs_smooth = qs_joint.sum(-1)
+        
+        return qs_smooth.todense(), (qs_smooth.todense(), qs_joint)
+
+    # seq_qs will contain a sequence of smoothed marginals and joints
+    _, seq_qs = lax.scan(
+        step_fn,
+        qs_last,
+        jnp.arange(qs_backward_cond.shape[0]),
+        reverse=True,
+        unroll=2
+    )
+
+    # we add the last filtered belief to smoothed beliefs
+
+    qs_smooth_all = jnp.concatenate([seq_qs[0], jnp.expand_dims(qs_last, 0)], 0)
+    return qs_smooth_all, seq_qs[1]
+
+
+def smoothing_ovf(filtered_post, B, past_actions):
+    assert len(filtered_post) == len(B)
+    nf = len(B)  # number of factors
+    joint = lambda b, qs, f: joint_dist_factor(b, qs, past_actions[..., f])
+    # marginals_and_joints = jtu.tree_map(
+    #     joint, B, filtered_post, list(range(nf)))
+
+    marginals_and_joints = []
+    for b, qs, f in zip(B, filtered_post, list(range(nf))):
+        marginals_and_joints_f = joint(b, qs, f)
+        marginals_and_joints.append(marginals_and_joints_f)
+
+    return marginals_and_joints
+
+
     
